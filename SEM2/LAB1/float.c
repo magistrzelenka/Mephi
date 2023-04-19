94aa1b13a55c8caf14bde66c847918aa11ccc577
magistrzelenka <87072205+magistrzelenka@users.noreply.github.com>
2023-04-19 09:27:20 +0300

    Create float.c

---
+++ SEM2/LAB1/float.c
@@ -0,0 +1,46 @@
+void* createF() {
+    void* res = malloc(sizeof(Float));
+    (*((Float*) res)).ring = Create(sizeof(Float), sumF, /*minusF,*/ multF, skalmultF);
+    return res;
+}
+
+void* sumF(void* F1, void* F2) {
+    void* res = createF();
+    (*((Float*) res)).f = (*((Float*) F1)).f + (*((Float*) F2)).f;
+	return res;
+}
+/*
+void* minusF(void* F1, void* F2) {
+	void* res = createF();
+    (*((Float*) res)).f = (*((Float*) F1)).f - (*((Float*) F2)).f;
+	return res;
+}
+*/
+
+void* multF(void* F1, void* F2) {
+	void* res = createF();
+    (*((Float*) res)).f = (*((Float*) F1)).f * (*((Float*) F2)).f;
+	return res;
+}
+
+void* skalmultF(void* F, double x) {
+    void* res = createF();
+    (*((Float*) res)).f = (*((Float*) F)).f * x;
+	return res;
+}
+
+
+Float gen_Float() {
+    Float F;
+    //F.ring = Create(sizeof(Float), sumF, minusF, multF, skalmultF);
+    F = *((Float*) createF());
+    F.f = randfloatfrom(-100, 100);
+
+    return F;
+}
+
+
+void print_Float(Float F) {
+    printf("%6.2f", F.f);
+}
+
