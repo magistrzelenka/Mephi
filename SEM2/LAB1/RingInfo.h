76ce1f0240647abff8b6cf63f566b73193e01ed4
magistrzelenka <87072205+magistrzelenka@users.noreply.github.com>
2023-04-19 09:39:31 +0300

    Update RingInfo.h

--- SEM2/LAB1/RingInfo.h
+++ SEM2/LAB1/RingInfo.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,21 +14,3 @@ struct RingInfo {
 };
 
 struct RingInfo* Create(size_t size, void* (*sum)(void*, void*), /*void* (*minus)(void*),*/ void* (*mult)(void*, void*), void* (*skalmult)(void*, double));
-
-
-//--------------------------------------------------------------------
-//RingInfo.c
-
-struct RingInfo* Create(size_t size, void* (*sum)(void*, void*), /*void* (*minus)(void*),*/ void* (*mult)(void*, void*), void* (*skalmult)(void*, double)) {
-	struct RingInfo* ringInfo = malloc(sizeof(struct RingInfo));
-	//ringInfo->zero = zero;
-	//ringInfo->one = one;
-	ringInfo->size = size;
-	ringInfo->sum = sum;
-	//ringInfo->minus = minus;
-	ringInfo->mult = mult;
-	ringInfo->skalmult = skalmult;
-}
-//struct RingInfo* Create(size_t size, void* zero, void* one, void* (*sum)(void*, void*), void* (*minus)(void*), void* (*mult)(void*, void*), void* (*skalmult)(void*, double)) {
-
-
