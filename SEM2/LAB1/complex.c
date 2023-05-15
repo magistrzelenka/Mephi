bd06d84d9d42e31b56a11c60d54d57cc0ba57e40
magistrzelenka <87072205+magistrzelenka@users.noreply.github.com>
2023-05-15 16:34:31 +0300

    Update complex.c

--- SEM2/LAB1/complex.c
+++ SEM2/LAB1/complex.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,7 +6,7 @@
 
 void* createC() {
     void* res = malloc(sizeof(Complex));
-    (*((Complex*) res)).ring = Create(sizeof(Complex), sumC, /*minusC,*/ multC, skalmultC);
+    (*((Complex*) res)).ring = Create(sizeof(Complex), sumC, multC, skalmultC);
     return res;
 }
 
@@ -15,14 +16,14 @@ void* sumC(void* C1, void* C2) {
 	(*((Complex*) res)).im = (*((Complex*) C1)).im + (*((Complex*) C2)).im;
 	return res;
 }
-/*
+
 void* minusC(void* C1, void* C2) {
 	void* res = createC();
 	(*((Complex*) res)).re = (*((Complex*) C1)).re - (*((Complex*) C2)).re;
 	(*((Complex*) res)).im = (*((Complex*) C1)).im - (*((Complex*) C2)).im;
 	return res;
 }
-*/
+
 
 void* multC(void* C1, void* C2) {
     void* res = createC();
@@ -41,7 +42,7 @@ void* skalmultC(void* C, double x) {
 
 Complex gen_Complex() {
     Complex C;
-    C.ring = Create(sizeof(Complex), sumC, /*minusC,*/ multC, skalmultC);
+    C.ring = Create(sizeof(Complex), sumC, multC, skalmultC);
     C.re = randdoublefrom(-100.0, 100.0);
     C.im = randdoublefrom(-100.0, 100.0);
     return C;
@@ -54,3 +55,4 @@ void print_Complex(Complex C) {
     else
         printf("%6.2lf-%6.2lf*i", C.re,  C.im*(-1));
 }
+*/
